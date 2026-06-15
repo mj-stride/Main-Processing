2d50a54e5e64d2a5745a9cf0f8a44933a69a2d2b
Matt <raynermatt1245@gmail.com>
2026-06-15 09:41:52 +0800

    feat: zip exporting
    Makes use of ZIP compression to handle multiple CSV files in different directories.

--- 4. Report Generator/Report Generator/Controllers/ReportController.cs
+++ 4. Report Generator/Report Generator/Controllers/ReportController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 using Microsoft.AspNetCore.Mvc;
+using System.IO.Compression;
 using Report_Generator.Models;
 using Report_Generator.Services;
 
@@ -44,86 +45,85 @@ namespace Report_Generator.Controllers
 
             var dataProcessor = new DataProcessorService();
 
-            foreach(var survey in vehicleDirs)
+            using var memory = new MemoryStream();
+            using (var zip = new ZipArchive(memory, ZipArchiveMode.Create, true))
             {
-                Console.WriteLine($"\nPROCESSING: {survey.Region} | {survey.RoadName} | {survey.SurveyDate} | {survey.VehicleType}");
-
-                var surveyTripData = new List<TripData>();
-                var surveyFiles = files.Where(f => f.FileName.StartsWith(survey.SegmentAnalysisPath)).ToList();
-
-                foreach (var file in surveyFiles)
+                foreach (var survey in vehicleDirs)
                 {
-                    if (file.FileName.EndsWith(".csv"))
+                    Console.WriteLine($"PROCESSING: {survey.Region} | {survey.RoadName} | {survey.SurveyDate} | {survey.VehicleType}");
+
+                    var surveyTripData = new List<TripData>();
+                    var surveyFiles = files.Where(f => f.FileName.StartsWith(survey.SegmentAnalysisPath)).ToList();
+
+                    foreach (var file in surveyFiles)
                     {
-                        // Filter .csv files with pattern: {TripNo}_{Anything}-NB/SB.csv
-                        string csvFileName = System.IO.Path.GetFileNameWithoutExtension(file.FileName);
-                        string pattern = @"^(\d+)_.*-(NB|SB|EB|WB)$";
-                        Match match = Regex.Match(csvFileName, pattern, RegexOptions.IgnoreCase);
-
-                        if (match.Success)
+                        if (file.FileName.EndsWith(".csv"))
                         {
-                            using (var stream = file.OpenReadStream())
+                            // Filter .csv files with pattern: {TripNo}_{...}-NB/SB/EB/WB.csv
+                            string csvFileName = System.IO.Path.GetFileNameWithoutExtension(file.FileName);
+                            string pattern = @"^(\d+)_.*-(NB|SB|EB|WB)$";
+                            Match match = Regex.Match(csvFileName, pattern, RegexOptions.IgnoreCase);
+
+                            if (match.Success)
                             {
-                                var data = _csvParser.ReadTripCsv(stream);
-
-                                // Get TripNo and Direction from filename
-                                int tripNo = int.Parse(match.Groups[1].Value);
-                                string direction = match.Groups[2].Value.ToUpper();
-
-                                if (direction == "EB") direction = "NB";
-                                if (direction == "WB") direction = "SB";
-
-                                // Get Period from parent directory
-                                string? dirPath = Path.GetDirectoryName(file.FileName);
-                                string period = dirPath != null ? Path.GetFileName(dirPath).ToUpper() : string.Empty;
-
-                                foreach (var row in data)
+                                using (var stream = file.OpenReadStream())
                                 {
-                                    row.TripNo = tripNo;
-                                    row.Direction = direction;
-                                    row.Period = period;
-                                }
+                                    var data = _csvParser.ReadTripCsv(stream);
 
-                                surveyTripData.AddRange(data);
+                                    // Get TripNo and Direction from filename
+                                    int tripNo = int.Parse(match.Groups[1].Value);
+                                    string direction = match.Groups[2].Value.ToUpper();
+
+                                    if (direction == "EB") direction = "NB";
+                                    if (direction == "WB") direction = "SB";
+
+                                    // Get Period from parent directory
+                                    string? dirPath = Path.GetDirectoryName(file.FileName);
+                                    string period = dirPath != null ? Path.GetFileName(dirPath).ToUpper() : string.Empty;
+
+                                    foreach (var row in data)
+                                    {
+                                        row.TripNo = tripNo;
+                                        row.Direction = direction;
+                                        row.Period = period;
+                                    }
+
+                                    surveyTripData.AddRange(data);
+                                }
                             }
                         }
                     }
-                }
 
-                if (surveyTripData.Any())
-                {
-                    // Directional Averages Test
-                    var directionalAverages = dataProcessor.CalculateDirectionalAverages(surveyTripData);
-                    string desktopPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
-                    string csvSavePath = Path.Combine(desktopPath, survey.Region, survey.RoadName, survey.SurveyDate, survey.VehicleType, "DirectionalAverages");
-                    if (!Directory.Exists(csvSavePath))
+                    if (surveyTripData.Any())
                     {
-                        Directory.CreateDirectory(csvSavePath);
+                        // Directional Averages
+                        var directionalAverages = dataProcessor.CalculateDirectionalAverages(surveyTripData);
+                        string[] periods = { "AM", "MID", "PM" };
+
+                        foreach (var period in periods)
+                        {
+                            string DirAvg = _csvExport.GenerateDirectionalAveragesCsv(directionalAverages, period);
+                            string zipEntryName = $"{survey.Region}/{survey.RoadName}/{survey.SurveyDate}/{survey.VehicleType}/{period}_DirectionalAverages.csv";
+                            var zipEntry = zip.CreateEntry(zipEntryName, CompressionLevel.Fastest);
+
+                            using (var entryStream = zipEntry.Open())
+                            using (var streamWriter = new StreamWriter(entryStream))
+                            {
+                                streamWriter.Write(DirAvg);
+                            };
+                        }
+
+                        // Segment Averages
+                        var segmentAverages = dataProcessor.CalculateSegmentAverages(surveyTripData);
+                        //Console.WriteLine($"Generated {segmentAverages.Count} Segment Averages.");
                     }
-                    string[] periods = { "AM", "MID", "PM" };
-
-                    foreach (var period in periods)
-                    {
-                        string DirAvg = _csvExport.GenerateDirectionalAveragesCsv(directionalAverages, period);
-                        string csvDirAvgFile = Path.Combine(csvSavePath, $"{period}_DirectionalAverages.csv");
-                        System.IO.File.WriteAllText(csvDirAvgFile, DirAvg);
-                        Console.WriteLine($"CSV SAVED on: {csvDirAvgFile}");
-                    }
-
-                    // Segment Averages Test
-                    var segmentAverages = dataProcessor.CalculateSegmentAverages(surveyTripData);
-                    Console.WriteLine($"Generated {segmentAverages.Count} Segment Averages.");
-
-                    // Delay Causes Summary Test
-                    //foreach (var seg in segmentAverages.Where(s => !string.IsNullOrEmpty(s.DelayCausesSummary)))
-                    //{
-                    //    Console.WriteLine($"Segment {seg.From} to {seg.To} Delays: {seg.DelayCausesSummary}");
-                    //}
-
                 }
             }
 
-            return Ok("Files Sent!!");
+            memory.Position = 0;
+            string zipName = $"Report_{DateTime.Now:yyyyMMdd}.zip";
+
+            return File(memory.ToArray(), "application/zip", zipName);
         }
     }
 }
