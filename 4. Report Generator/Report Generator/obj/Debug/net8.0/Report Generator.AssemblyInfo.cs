9f40a0a12fe5e1365ba00ec108fd1936ca811f9b
Matt <raynermatt1245@gmail.com>
2026-06-22 15:50:19 +0800

    changed zip icon

--- 4. Report Generator/Report Generator/obj/Debug/net8.0/Report Generator.AssemblyInfo.cs
+++ 4. Report Generator/Report Generator/obj/Debug/net8.0/Report Generator.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Report Generator")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f57297838433eceabcbdb9c1a0394bca3b0d9f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49167376b181088c43927c33e25a00d3bebced1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Report Generator")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Report Generator")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
